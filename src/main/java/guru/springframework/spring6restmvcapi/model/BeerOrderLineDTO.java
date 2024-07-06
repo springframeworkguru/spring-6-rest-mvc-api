6092c40e95f74c2b1550e8ba8bf4be686a6de022
John Thompson <john@springframework.guru>
2024-07-06 15:08:11 -0400

    updates

---
+++ src/main/java/guru/springframework/spring6restmvcapi/model/BeerOrderLineDTO.java
@@ -0,0 +1,27 @@
+package guru.springframework.spring6restmvcapi.model;
+
+import jakarta.validation.constraints.Min;
+import lombok.Builder;
+import lombok.Data;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+@Builder
+@Data
+public class BeerOrderLineDTO {
+    private UUID id;
+
+    private Long version;
+    private Timestamp createdDate;
+    private Timestamp lastModifiedDate;
+
+    private BeerDTO beer;
+
+    @Min(value = 1, message = "Quantity On Hand must be greater than 0")
+    private Integer orderQuantity;
+    private Integer quantityAllocated;
+}
---
+++ src/main/java/guru/springframework/spring6restmvcapi/model/BeerOrderShipmentDTO.java
@@ -0,0 +1,26 @@
+package guru.springframework.spring6restmvcapi.model;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Data;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+@Data
+@Builder
+public class BeerOrderShipmentDTO {
+
+    private UUID id;
+
+    private Long version;
+
+    @NotBlank
+    private String trackingNumber;
+
+    private Timestamp createdDate;
+    private Timestamp lastModifiedDate;
+}
