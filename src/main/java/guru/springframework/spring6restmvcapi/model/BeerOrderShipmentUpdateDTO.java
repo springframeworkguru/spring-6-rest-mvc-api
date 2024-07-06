bca6d1e09115fe11b52d8175e6762ec00d5b0cdd
John Thompson <john@springframework.guru>
2024-07-06 13:52:09 -0400

    initial project add

---
+++ src/main/java/guru/springframework/spring6restmvcapi/model/BeerOrderShipmentUpdateDTO.java
@@ -0,0 +1,20 @@
+package guru.springframework.spring6restmvcapi.model;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BeerOrderShipmentUpdateDTO {
+
+    @NotBlank
+    private String trackingNumber;
+}
