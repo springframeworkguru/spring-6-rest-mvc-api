30ec6b157f34ae3ff751be8807fe2c31ffe6b872
John Thompson <john@springframework.guru>
2024-07-06 14:59:01 -0400

    revert

--- src/main/java/guru/springframework/spring6restmvcapi/model/BeerOrderLineStatus.java
+++
@@ -1,8 +0,0 @@
-package guru.springframework.spring6restmvcapi.model;
-
-/**
- * Created by jt, Spring Framework Guru.
- */
-public enum BeerOrderLineStatus {
-    NEW, COMPLETE
-}
--- src/main/java/guru/springframework/spring6restmvcapi/model/BeerStyle.java
+++
@@ -1,8 +0,0 @@
-package guru.springframework.spring6restmvcapi.model;
-
-/**
- * Created by jt, Spring Framework Guru.
- */
-public enum BeerStyle {
-    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
-}
